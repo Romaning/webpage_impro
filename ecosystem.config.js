c32b337c241c7a5e03d079c8342e8d004b44417d
Roman Franco Valero Calle <romanfranco767@gmail.com>
2023-09-18 03:13:44 +0000

    add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,24 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
+    script: 'npm start',
+    /*watch: '.'*/
+  }/*, {
     script: './service-worker/',
     watch: ['./service-worker']
-  }],
+  }],*/
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
+      key : 'server_roman_aws_key.pem'
+      user : 'ubuntu',
+      host : '18.222.14.156',
       ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      repo : 'https://github.com/Romaning/webpage_impro',
+      path : '/opt/PROJECT',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
     }
   }
 };
